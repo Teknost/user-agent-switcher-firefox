551ff58e7a2632cbd06fed2ccbc78c9243fea3f0
Teknost <x86Daddy@gmail.com>
2021-06-25 13:17:14 -0500

    Add GoogleBot

--- source/background.js
+++ source/background.js
@@ -3,6 +3,7 @@
 // code based on https://github.com/mdn/webextensions-examples/tree/master/user-agent-rewriter
 const uaStrings = {
   "Firefox": "",
+  "Google Bot": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
   "Chrome": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
   "IE 11": "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0;  rv:11.0) like Gecko",
   "IE 10": "Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)",
